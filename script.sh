2418af27c4d04bedc022bcfc006246c2bfcf8bac
Masha Shinkareva <ms.shinkareva@yandex.ru>
2021-08-30 23:03:49 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 echo "# demo-pipeline" >> README.md
 git init
 git add README.md
-git commit -m "first commit"
+git commit -m "second commit"
 git remote add origin git@github.com:mshinkareva/sjp.git
 git push -u origin master
